05d6fef1b5953d253911ed35c5539803c0c0dd17
shivamsr7 <shivam25061994@gmail.com>
2026-07-26 20:49:21 +0530

    Account Page

--- src/features/customers/pages/AccountPage.tsx
+++ src/features/customers/pages/AccountPage.tsx
@@ -1,11 +1,49 @@
 import LoungeDashboard from "../components/lounge/LoungeDashboard";
 
+import MobileLounge from "../components/lounge/mobile/MobileLounge";
+
+
 
 export default function AccountPage(){
 
+
 return (
 
+<>
+
+
+{/* Desktop */}
+
+<div className="
+hidden
+lg:block
+">
+
 <LoungeDashboard />
+
+</div>
+
+
+
+
+
+
+
+{/* Mobile */}
+
+<div className="
+block
+lg:hidden
+">
+
+<MobileLounge />
+
+</div>
+
+
+
+
+</>
 
 );
 
