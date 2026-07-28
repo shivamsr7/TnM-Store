c0f586f721e9f48ec65eafa4b03c55f53d137cdc
shivamsr7 <shivam25061994@gmail.com>
2026-07-28 15:49:04 +0530

    shop collection

--- src/features/products/pages/AddProductPage.tsx
+++
@@ -1,9 +0,0 @@
-import ProductForm from "../components/ProductForm";
-
-export default function AddProductPage() {
-  return (
-    <div className="container mx-auto max-w-7xl p-6">
-      <ProductForm />
-    </div>
-  );
-}
