54d06843328cc9d50496a5b2256272be4ca2af2a
shivamsr7 <shivam25061994@gmail.com>
2026-08-02 12:15:59 +0530

    product details

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import Header from "@/features/header/components/Header";
 import HeroSection from "@/features/home/hero/components/HeroSection";
 import HangingCategories from "@/features/categories/components/HangingCategories";
 import Footer from "@/shared/components/footer/Footer";
-import CartDrawer from "@/features/cart/components/CartDrawer";
+
 export default function App() {
 
   return (
